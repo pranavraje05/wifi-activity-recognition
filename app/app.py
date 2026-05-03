97dc90ae49c6c729a4a424e9d6cdaeeb2e400369
pranavraje05 <pranavraje05@gmail.com>
2026-05-03 14:37:34 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,19 +2,23 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-from src.model import train_model
+from src.model import train_and_evaluate
 
+
+# Title
 st.title("WiFi-Based Human Activity Recognition")
 
-# Run training
-model, acc, y_test, preds = train_model()
+# Run model
+model, acc, y_test, preds = train_and_evaluate()
 
-# Confusion matrix
+# Create confusion matrix
 cm = confusion_matrix(y_test, preds)
 
+# Show accuracy
 st.subheader("📊 Model Performance")
 st.write(f"Accuracy: {acc:.2f}")
 
+# Show confusion matrix
 st.subheader("Confusion Matrix")
 fig, ax = plt.subplots()
 ax.imshow(cm, cmap='viridis')
@@ -22,12 +26,13 @@ ax.set_xlabel("Predicted")
 ax.set_ylabel("Actual")
 st.pyplot(fig)
 
-# Prediction upload
+# Upload CSV for prediction
 st.subheader("📂 Upload CSV for Prediction")
 file = st.file_uploader("Upload CSV file")
 
 if file:
     data = pd.read_csv(file)
     prediction = model.predict(data)
+
     st.write("Predictions:")
     st.write(prediction)
