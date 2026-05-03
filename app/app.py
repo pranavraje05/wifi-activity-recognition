a3deac8a4c4d7b0621b034135fbfdffaa6a38d49
pranavraje05 <pranavraje05@gmail.com>
2026-05-03 14:41:45 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,28 +5,24 @@ from sklearn.metrics import confusion_matrix
 from src.model import train_and_evaluate
 
 
-# Title
 st.title("WiFi-Based Human Activity Recognition")
 
-# Run model
 model, acc, y_test, preds = train_and_evaluate()
 
-# Create confusion matrix
-cm = confusion_matrix(y_test, preds)
-
-# Show accuracy
 st.subheader("📊 Model Performance")
 st.write(f"Accuracy: {acc:.2f}")
 
-# Show confusion matrix
 st.subheader("Confusion Matrix")
+
+cm = confusion_matrix(y_test, preds)
+
 fig, ax = plt.subplots()
 ax.imshow(cm, cmap='viridis')
 ax.set_xlabel("Predicted")
 ax.set_ylabel("Actual")
 st.pyplot(fig)
 
-# Upload CSV for prediction
+
 st.subheader("📂 Upload CSV for Prediction")
 file = st.file_uploader("Upload CSV file")
 
