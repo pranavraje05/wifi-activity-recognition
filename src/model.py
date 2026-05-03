e2e6f015ea86243b12fcbecb1afbc1a66f8fb0df
pranavraje05 <pranavraje05@gmail.com>
2026-05-03 14:12:37 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,32 +1,31 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 import os
 
-def load_data():
-    base_path = os.path.join("data", "UCI HAR Dataset")
 
-    # Training data
-    X_train = pd.read_csv(os.path.join(base_path, "train", "X_train.txt"), delim_whitespace=True, header=None)
-    y_train = pd.read_csv(os.path.join(base_path, "train", "y_train.txt"), header=None)
+def load_ucihar():
+    base_path = "data/UCI HAR Dataset"
 
-    # Testing data
-    X_test = pd.read_csv(os.path.join(base_path, "test", "X_test.txt"), delim_whitespace=True, header=None)
-    y_test = pd.read_csv(os.path.join(base_path, "test", "y_test.txt"), header=None)
+    X_train = pd.read_csv(os.path.join(base_path, "train/X_train.txt"), delim_whitespace=True, header=None)
+    y_train = pd.read_csv(os.path.join(base_path, "train/y_train.txt"), header=None)
 
-    print("X_train:", X_train.shape)
-    print("X_test:", X_test.shape)
+    X_test = pd.read_csv(os.path.join(base_path, "test/X_test.txt"), delim_whitespace=True, header=None)
+    y_test = pd.read_csv(os.path.join(base_path, "test/y_test.txt"), header=None)
 
     return X_train, X_test, y_train.values.ravel(), y_test.values.ravel()
 
 
 def train_model():
-    X_train, X_test, y_train, y_test = load_data()
+    X_train, X_test, y_train, y_test = load_ucihar()
 
     model = RandomForestClassifier(n_estimators=100)
+
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
+
     acc = accuracy_score(y_test, preds)
 
     return model, acc, y_test, preds
